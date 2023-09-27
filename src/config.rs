cae0e32f1cc02b51baa0f5998f084091daeadeb2
FriskIsGit <friskisgit@users.noreply.github.com>
2023-09-27 22:46:45 +0100

    first runnable version

---
+++ src/config.rs
@@ -0,0 +1,19 @@
+use std::fs;
+use serde::Serialize;
+use serde::Deserialize;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config{
+    pub api_key: String,
+    pub include_adult: bool,
+    pub enable_cache: bool
+}
+
+impl Config{
+    pub fn read_config(path: &str) -> Config{
+        let contents: String = fs::read_to_string(path).expect("File not found");
+        let a_str: &str = contents.as_str();
+        let config: Config = serde_json::from_str(a_str).unwrap();
+        return config
+    }
+}
