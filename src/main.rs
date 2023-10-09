238b65be9f2dabc44e3ea4d98cfa14cefd4d1955
Kihau <michal.haldas@gmail.com>
2023-10-10 01:34:26 +0200

    Add TODO

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,13 @@ use eframe::AppCreator;
 use std::any::Any;
 use std::time::Duration;
 
-//TODO add + in every row with a production or drag and drop to move items to the center panel
-//TODO load images on a separate thread so it doesn't lag ui, display buffering circle(egui does it already)?,
-// - perhaps make the request on a separate thread in the first place
-//TODO temporary workaround for wininit compilation time(15s)?
-//TODO add exception handling to requests to avoid crashing the app in case something goes wrong
+// TODO: Add + in every row with a production or drag and drop to move items to the center panel
+// TODO: Load images on a separate thread so it doesn't lag ui, display buffering circle(egui does it already)?
+//       Perhaps make the request on a separate thread in the first place
+// TODO: Temporary workaround for winit compilation time(15s)?
+// TODO: Add exception handling to requests to avoid crashing the app in case something goes wrong
+// TODO: Add scaling to the posters and trim long descriptions, don't artificially stretch the left
+//       panel when production entries are added.
 
 fn main() {
     println!("Running!");
