ebbf1521f752f8bda9f1de223302685b09475a7a
FriskIsGit <friskisgit@users.noreply.github.com>
2023-10-13 23:59:08 +0200

    expanded credits

--- src/credits.rs
+++ src/credits.rs
@@ -1,11 +1,37 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct MovieCredits {}
+pub struct Credits {
+    id: u32,
+    //simplify it to cast at some point
+    cast: Vec<Actor>,
+    //don't deserialize crew
+}
 
-impl MovieCredits {}
+impl Credits {
+    pub fn parse(json: &str) -> Credits {
+        serde_json::from_str(json).expect("Failed to deserialize a Credits object")
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct SeriesCredits {}
+pub struct Actor {
+    character: String,
+    name: String,
+    original_name: String,
+    credit_id: u32,
+    gender: u32,
+    adult: bool,
+}
 
-impl SeriesCredits {}
+impl Actor {
+    pub fn get_gender(&self) -> &str{
+        return match self.gender {
+            0 => "Unset",
+            1 => "Female",
+            2 => "Male",
+            3 => "Non-binary",
+            _ => "Unreachable"
+        }
+    }
+}
